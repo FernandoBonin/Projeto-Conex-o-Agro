46810eccdde622ca3eb36a1239845d228d7c9ae6
Fernando Bonin <fernando.bonin@hotmail.com.br>
2022-08-16 14:27:59 -0300

    search engine adjustment

--- assets/js/scriptProduct.js
+++ assets/js/scriptProduct.js
@@ -1,6 +1,16 @@
 "use strict";
 
-// CLONE TEMPLATE
+// GET ELEMENTS
+const category = document.getElementById("category");
+const searchBar = document.getElementById("searchBar");
+const elementFilterValue = document.getElementById("filterValue");
+
+// VARIABLES
+let inputProdQty;
+let resultQtyInput;
+let resultCurrency;
+
+// CREAT TEMPLATE
 function creatTemplate(productArr) {
   if ("content" in document.createElement("template")) {
     const cardsArea = document.getElementById("cardsArea");
@@ -18,21 +28,48 @@ function creatTemplate(productArr) {
       let getData = temp.querySelector("img").parentElement.parentElement;
       getData.setAttribute("data-category", item.categoria);
       getData.setAttribute("data-name", item.nome);
+
       cardsArea.appendChild(temp);
     });
   } else {
     console.error("Seu navegador não suporta template");
   }
+
+  // GET VALUE PRODUCTS
+  inputProdQty = document.querySelectorAll(".inputProdQty");
+
+  inputProdQty.forEach((item) => {
+    item.addEventListener("change", getValueInput);
+    function getValueInput() {
+      let totalProduct =
+        item.parentElement.parentElement.parentElement.lastElementChild;
+      let dataName =
+        item.parentElement.parentElement.parentElement.parentElement.getAttribute(
+          "data-name"
+        );
+      let valueInput = item.value;
+
+      products.forEach((item) => {
+        if (dataName == item.nome) {
+          resultQtyInput = valueInput * item.valor;
+          resultCurrency = resultQtyInput.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          });
+        }
+      });
+      if (valueInput > 0) {
+        totalProduct.style.color = "black";
+        totalProduct.innerHTML = "Total: " + resultCurrency;
+      } else {
+        totalProduct.style.color = "red";
+        totalProduct.innerHTML = "Digite um valor válido";
+      }
+    }
+  });
 }
 
 creatTemplate(products);
-
-// GET ELEMENTS
-const category = document.getElementById("category");
-const searchBar = document.getElementById("searchBar");
-let allCards = document.querySelectorAll(".cardAreaClass");
-let inputProdQty = document.querySelectorAll(".inputProdQty");
-const elementFilterValue = document.getElementById("filterValue");
 
 // FORMAT NAME
 function formatName(name) {
@@ -49,45 +86,59 @@ function formatName(name) {
 // EVENT SELECT CATEGORY
 category.addEventListener("change", selectCategory);
 function selectCategory() {
+  let categoryValue = category.value;
+  let productsCategory = [];
+  let allCards = document.querySelectorAll(".cardAreaClass");
+
   allCards.forEach((item) => {
-    item.classList.remove("d-none");
-    let dataCategory = item.getAttribute("data-category");
-    if (dataCategory != category.value) {
-      item.classList.add("d-none");
-    }
-    if (category.value == "Categorias") {
-      item.classList.remove("d-none");
+    item.remove();
+  });
+
+  if (categoryValue == "Categorias") {
+    return creatTemplate(products);
+  }
+
+  products.forEach((item) => {
+    if (categoryValue == item.categoria) {
+      return productsCategory.push(item);
     }
   });
+  creatTemplate(productsCategory);
 }
 
 // EVENT SEARCH BAR
 searchBar.addEventListener("keyup", searchProduct);
 function searchProduct(event) {
   let searchBarValue = formatName(event.target.value);
+  let allCards = document.querySelectorAll(".cardAreaClass");
+  let searchBarArr = [];
+
   allCards.forEach((item) => {
-    let dataName = formatName(item.getAttribute("data-name"));
-    if (!dataName.includes(searchBarValue)) {
-      item.classList.add("d-none");
-    } else {
-      item.classList.remove("d-none");
+    item.remove();
+  });
+
+  products.forEach((item) => {
+    let productsName = formatName(item.nome);
+    if (productsName.includes(searchBarValue)) {
+      return searchBarArr.push(item);
     }
   });
+  creatTemplate(searchBarArr);
 }
 
 // EVENT FILTER VALUE
-elementFilterValue.addEventListener("change", () => {
+elementFilterValue.addEventListener("change", filterValue);
+function filterValue() {
   let selectValue = elementFilterValue.value;
   let productusOrderly;
-  let cardsClass = document.querySelectorAll(".cardAreaClass");
+  let allCards = document.querySelectorAll(".cardAreaClass");
 
-  cardsClass.forEach((item) => {
+  allCards.forEach((item) => {
     item.remove();
   });
 
   if (selectValue == "") {
-    creatTemplate(products);
-    return;
+    return creatTemplate(products);
   }
 
   if (selectValue == 1) {
@@ -110,37 +161,4 @@ elementFilterValue.addEventListener("change", () => {
     });
   }
   creatTemplate(productusOrderly);
-});
-
-// GET PRODUCT VALUE
-let resultQtyInput;
-let resultCurrency;
-inputProdQty.forEach((item) => {
-  item.addEventListener("change", () => {
-    console.log(item);
-    let totalProduct =
-      item.parentElement.parentElement.parentElement.lastElementChild;
-    let dataName =
-      item.parentElement.parentElement.parentElement.parentElement.getAttribute(
-        "data-name"
-      );
-    let valueInput = item.value;
-
-    products.forEach((item) => {
-      if (dataName == item.nome) {
-        resultQtyInput = valueInput * item.valor;
-        resultCurrency = resultQtyInput.toLocaleString("pt-br", {
-          style: "currency",
-          currency: "BRL",
-        });
-        if (valueInput >= 0) {
-          totalProduct.style.color = "black";
-          totalProduct.innerHTML = "Total: " + resultCurrency;
-        } else {
-          totalProduct.style.color = "red";
-          totalProduct.innerHTML = "Digite um valor válido";
-        }
-      }
-    });
-  });
-});
+}
